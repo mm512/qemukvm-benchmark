4fb67b6962a54380ae37fc3a3162818d837c2353
Astral Cosmonaut <astralcosmonaut@gmail.com>
2014-07-03 01:41:56 +0000

    Main code reorganize

--- qemukvm-benchmark/main.c
+++ qemukvm-benchmark/main.c
@@ -8,13 +8,58 @@ void usage(void)
     printf("Usage:\n\tqemukvm-benchmark [-l or -h] source_path\n-l - low compression\n-h high compression");
 }
 
+void get_input_name(char *file_name, char **argv)
+{
+    if (argv[1][0] == '-') {
+        strcpy(file_name, argv[2]);
+    } else {
+        strcpy(file_name, argv[1]);
+    }
+}
+
+int get_compression_level(char *argv)
+{
+    if (argv[0] == '-') {
+        if (argv[1] == 'l') {
+            return LOW_COMPRESSION;
+        } else {
+            return HIGH_COMPRESSION;
+        }
+    }
+
+    return HIGH_COMPRESSION;
+}
+
+int run_benchmark(FILE *source, char *file_name, int library, int level, int iterations)
+{
+    FILE *archfile;
+    char arch_file_name[100];
+    strcpy(arch_file_name, file_name);
+
+    switch(library) {
+    case LIB_ZLIB:
+        strcat(arch_file_name, ".zlib");
+        archfile = fopen(arch_file_name, "w+");
+        if (!archfile) {
+            puts("Error: problem with opening archive file.");
+            return 1;
+        }
+        run_zlib(source, archfile, level, iterations);
+        fclose(archfile);
+        break;
+    default:
+        return 1;
+    }
+
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     int iterations = 1;
-    int arg_index = 1;
     int level = HIGH_COMPRESSION;
-    FILE *infile, *archfile;
-    char arch_file_name[100];
+    FILE *infile;
+    char input_file_name[100];
 
     puts("*** QEMU KVM performance benchmark ***\n");
 
@@ -24,40 +69,28 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    // Level.
-    if (argv[1][0] == '-') {
-        if (argv[1][1] == 'l') {
-            level = LOW_COMPRESSION;
-            puts("Compression level set to low.");
-        } else {
-            level = HIGH_COMPRESSION;
-            puts("Compression level set to high.");
-        }
+    get_input_name(input_file_name, argv);
 
-        ++arg_index;
+    // Level.
+    level = get_compression_level(argv[1]);
+    if (level == LOW_COMPRESSION) {
+        puts("Compression level set to low.");
     } else {
         puts("Compression level set to high.");
     }
 
-    infile = fopen(argv[arg_index], "r");
+    // Open input file.
+    infile = fopen(input_file_name, "r");
     if (!infile) {
         puts("Error: problem with opening input file.");
         return 1;
     }
 
-    strcpy(arch_file_name, argv[arg_index]);
-    strcat(arch_file_name, ".zlib");
-    archfile = fopen(arch_file_name, "w+");
-    if (!archfile) {
-        puts("Error: problem with opening archive file.");
-        fclose(infile);
-        return 1;
-    }
+    // Run zlib.
+    run_benchmark(infile, input_file_name, LIB_ZLIB, level, iterations);
 
-    run_zlib(infile, archfile, level, iterations);
 
     fclose(infile);
-    fclose(archfile);
     return 0;
 }
 
